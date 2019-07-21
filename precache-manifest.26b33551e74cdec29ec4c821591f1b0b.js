2dd457925d6411e813ca2698b0f1233ffc660cee
Peter Mueller <peter.mueller@ascaion.com>
2019-07-21 18:02:35 +0200

    Gh pages

---
+++ precache-manifest.26b33551e74cdec29ec4c821591f1b0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5724bbc5efa0b7066c70ede7b8a98f7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46319112bb6e28d8c163",
+    "url": "/static/css/main.ab97f8ae.chunk.css"
+  },
+  {
+    "revision": "c82da39d03a91ac34e67",
+    "url": "/static/js/2.ccbf4cb7.chunk.js"
+  },
+  {
+    "revision": "46319112bb6e28d8c163",
+    "url": "/static/js/main.a3faa258.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
